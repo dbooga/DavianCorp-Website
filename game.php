68768b84549c0b22b1bf0ed2115490e117ee2bc2
David Nguyen <nguyend0703@yahoo.com>
2015-07-27 18:18:21 -0700

    Added game.php

---
+++ game.php
@@ -0,0 +1,15 @@
+<?php
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Game</title>
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+</head>
+<body>
+	Hello DC
+
+</body>
+</html>
